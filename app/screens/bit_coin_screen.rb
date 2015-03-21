fcd635384e4b8e24425faeec5fd3cbbbd3a47367
Jamon Holmgren <jamon@clearsightstudio.com>
2015-03-21 16:54:52 -0700

    Changed it to be more like React.js

--- app/screens/bit_coin_screen.rb
+++ app/screens/bit_coin_screen.rb
@@ -3,36 +3,50 @@ class BitCoinScreen < PM::Screen
   stylesheet BitCoinScreenStylesheet
 
   def on_load
-    @bitcoin_prices = {}
-    @bitcoin_price = nil
-    @last_fetched = nil
-    @currency = "USD"
-    @currencies = [ "USD", "AUD", "CAD", "EUR" ]
+    @state = {
+      bitcoin_prices: {},
+      last_fetched: nil,
+      currency: "USD",
+      currencies: [ "USD", "AUD", "CAD", "EUR" ],
+    }
+    set_state(@state)
+    load_prices
+  end
 
-    append(UILabel, :bitcoin_price).data("Loading...")
-    append(UILabel, :last_fetched_date).data("...")
-    append(UIButton, :cycle_currency).data(@currency).on(:tap) do
-      # Get next currency
-      @currency = @currencies.rotate(@currencies.index(@currency) + 1).first
-      @bitcoin_price = @bitcoin_prices[@currency]["global_averages"]["last"]
-      find(:bitcoin_price).data("#{@bitcoin_price} #{@currency}")
-      find(:cycle_currency).data(@currency)
+  def set_state(state)
+    if find(:bitcoin_price).length == 0
+      append(UILabel, :bitcoin_price)
+      append(UILabel, :last_fetched_date)
+      append(UIButton, :cycle_currency).on(:tap) do
+        rotate_currency
+      end
     end
 
-    load_prices
+    if state[:bitcoin_prices][state[:currency]]
+      bitcoin_price = state[:bitcoin_prices][state[:currency]]["global_averages"]["last"]
+    else
+      bitcoin_price = "Loading"
+    end
+
+    find(:bitcoin_price).data("#{bitcoin_price} #{state[:currency]}")
+    find(:last_fetched_date).data(state[:last_fetched_date])
+    find(:cycle_currency).data(state[:currency])
+  end
+
+  def rotate_currency
+    @state[:currency] = @state[:currencies].rotate(@state[:currencies].index(@state[:currency]) + 1).first
+    set_state(@state)
   end
 
   def load_prices
     AFMotion::JSON.get("https://api.bitcoinaverage.com/all") do |result|
       if result.success?
-        @bitcoin_prices = result.object
-        @bitcoin_price = @bitcoin_prices[@currency]["global_averages"]["last"]
-        find(:bitcoin_price).data("#{@bitcoin_price} #{@currency}")
-        find(:last_fetched_date).data(Time.now.strftime("%b %e, %l:%M %p"))
+        @state[:bitcoin_prices] = result.object
+        @state[:last_fetched_date] = Time.now.strftime("%b %e, %l:%M %p")
+        set_state(@state)
       else
         mp result
       end
     end
   end
-
 end
